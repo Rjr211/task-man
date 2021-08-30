28396bf54b931c4e15d29c7f50e0edcb9630dd14
Rob <robert.j1450@gmail.com>
2021-08-29 21:11:37 -0400

    Added test documents to collection

--- mongodb.js
+++ mongodb.js
@@ -12,8 +12,55 @@ MongoClient.connect(connectionURL, { useNewURLParser: true}, (error, client )=> 
 
    const db = client.db(databaseName)
 
-   db.collection('users').insertOne({
-       name: 'Johnny',
-       age: 34
-   })
+//    db.collection('users').insertOne({
+//        name: 'Johnny',
+//        age: 34
+//    }, (error, result) => {
+//         if (error) {
+//             return console.log("unable to insert user")
+//         }
+
+//         console.log(result.ops)
+//    })
+
+    // Reference the collection and pass in collection name
+    // Call insertMany and pass in array
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Joseph',
+    //         age: 65
+    //     },
+    //     {
+    //         name: 'Cal',
+    //         age: 35
+    //     }
+    // ], (error, result) => {
+    //     if (error){
+    //         return console.log('Unable to insert documents')
+    //     }
+    //     // If insertion is successful print array of documents
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Meeting with Alyx',
+            completed: true
+        },
+        {
+            description: 'Attend fashion show',
+            completed: true
+        },
+        {
+            description: 'Pickup check from office',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to successfully add documents')
+        }
+
+        return console.log(result.ops)
+    })
+
 })
